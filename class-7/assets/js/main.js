d8b4f86619161584f0b6b6752924a7b6e15c5e70
Sajalkumarkundu <sajalkundu1995@gmail.com>
2017-07-27 18:34:31 -0700

    class-7

---
+++ class-7/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function($){
+	$(".testimonial-box").owlCarousel({
+		items:1,
+		margin:20,
+		loop:true,
+		nav:true,
+		autoplay:true,
+		autoplay:1000,
+		navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	});
+});
